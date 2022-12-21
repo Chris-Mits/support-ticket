5f22031bb63f08a8eacf8ced9d2614e32d78915e
Chris Mitsiaris <cmitsiaris@atypon.com>
2022-12-21 16:45:18 +0200

    chore: post build fix

--- backend/server.js
+++ backend/server.js
@@ -19,14 +19,13 @@ app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
-  // Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')))
-  // Create a route
-  app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
   })
 } else {
-  app.get('/', (_, res) => {
+  app.get('/', (req, res) => {
     res.status(200).json({message: 'Welcome to the Support Desk API'})
   })
 }
